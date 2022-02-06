a3bf411819316c097540a3accfccf63dee4cf4dd
yseij <r0790156@student.thomasmore.be>
2022-02-06 16:56:02 +0100

    API-routes gebruiken om gegevens te verkrijgen

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,6 +1,16 @@
 import fs from "fs";
 import path from "path";
 
+function buildFeedbackPath() {
+  return path.join(process.cwd(), "data", "feedback.json");
+}
+
+function extractFeeback(filePath) {
+  const fileData = fs.readFileSync(filePath);
+  const data = JSON.parse(fileData);
+  return data;
+}
+
 function handler(req, res) {
   if (req.method === "POST") {
     const email = req.body.email;
@@ -12,16 +22,16 @@ function handler(req, res) {
       text: feedbackText,
     };
 
-    const filePath = path.join(process.cwd(), "data", "feedback.json");
-    const fileData = fs.readFileSync(filePath);
-    const data = JSON.parse(fileData);
+    const filePath = buildFeedbackPath();
+    const data = extractFeeback(filePath);
     data.push(newFeedback);
     fs.writeFileSync(filePath, JSON.stringify(data));
-
     res.status(201).json({ message: "Succes", feedback: newFeedback });
   } else {
+    const filePath = buildFeedbackPath();
+    const data = extractFeeback(filePath);
     res.status(200).json({
-      message: "this works!",
+      feedback: data,
     });
   }
 }
