9d27361c01ff559ba2382a9525ecd50ab67d1189
yseij <r0790156@student.thomasmore.be>
2022-02-06 15:19:15 +0100

    Het frontend-formulier voorbereiden

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = emailInpfeedbackInputRefutRef.current.value;
+
+    
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="email">Your email address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Send feedback</button>
+      </form>
     </div>
   );
 }
